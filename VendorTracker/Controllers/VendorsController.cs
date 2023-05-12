7f2b09b97832e126670d96c24a6130ef2b52d476
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-12 13:54:15 -0700

    Bugfix: change Show route to use HttpGet instead of Post. the vendor page now loads successfully

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -27,7 +27,7 @@ namespace VendorTracker.Controllers
       return RedirectToAction("Index");
     }    
 
-    [HttpPost("/vendors/{id}")]
+    [HttpGet("/vendors/{id}")]
     public ActionResult Show(int id)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
