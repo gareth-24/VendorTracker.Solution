5ec2352cf2497e74d0a64560682a44a81ab8965d
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-12 13:37:36 -0700

    WIP: routing for vendors/show to show the orders linked to a specific vendor

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -26,5 +26,29 @@ namespace VendorTracker.Controllers
       Vendor newVendor = new Vendor(vendorName);
       return RedirectToAction("Index");
     }    
+
+    [HttpPost("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderDate, int orderPrice)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderDate, orderPrice);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
